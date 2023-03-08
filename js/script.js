56dcd5a65592f6282b772441b9f72dbdea426d7f
davidukiri <124379550+davidukiri@users.noreply.github.com>
2023-03-08 15:54:55 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Ms Raffin
+// Created on: Mar 2022
+// Edited by David ukiri
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('multiply math').innerHTML = 'The area is = ' + ( 10*14) + 
+ 'cm<sup>2</sup>'
+  document.getElementById('add math').innerHTML = 'The perimeter is =  ' + ( 10+10+14+14) + 
+ 'cm<sup>2</sup>'
+
+}
+
